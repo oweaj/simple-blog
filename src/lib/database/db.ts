4e3a0fe163cb71d54ce5189a47438e1adf41ef30
oweaj <qwe1520156@naver.com>
2025-12-23 11:12:12 +0900

    fix: mongoose에서 MissingSchemaError 인한 스키마 모델 연결 시점 등록

--- src/lib/database/db.ts
+++ src/lib/database/db.ts
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import { Auth } from "../schemas/auth-schema";
+import { Blog } from "../schemas/blog-schema";
+import { Notice } from "../schemas/notice-schema";
 
 const connectDB = async () => {
   try {
     if (mongoose.connection.readyState >= 1) return;
     await mongoose.connect(process.env.MONGODB_URL as string);
     console.log("mongoDB 연결 성공");
+
+    mongoose.models.Auth || mongoose.model("Auth", Auth.schema);
+    mongoose.models.Blog || mongoose.model("Blog", Blog.schema);
+    mongoose.models.Blog || mongoose.model("Notice", Notice.schema);
   } catch (error) {
     console.log(error);
   }
